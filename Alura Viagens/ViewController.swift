47389e60798fb09bf5af9d08fe1abc745de046e6
Mauro Diniz <diniz.mauroaugusto@gmail.com>
2019-02-23 15:48:00 -0300

    19_02_23
    commit inicial, criando arquivos

---
+++ Alura Viagens/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alura Viagens
+//
+//  Created by Mauro Augusto Diniz on 23/02/19.
+//  Copyright © 2019 Mauro Augusto Diniz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
